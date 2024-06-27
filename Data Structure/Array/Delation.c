440e09edc1791d655c5b2737d19172086f21c392
Tahan-Attin <tahanattin5@gmail.com>
2024-06-28 02:22:10 +0600

    Update Delation.c

--- Data Structure/Array/Delation.c
+++ Data Structure/Array/Delation.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
+void input_array(int arr[] ,int x)
+{
+    for(int i=0;i<x;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+}
+void delete_array(int n,int p,int arr[])
+{
+    for(int i=p-1;i<n-1;i++)
+    {
+        arr[i]=arr[i+1];
+    }
+    n=n-1;
+    for(int j=0;j<n;j++)
+    {
+        printf("%d",arr[j]);
+    }
+}
+int main()
+{
+    int n,p,m;
+    int arr[100];
+    printf("enter the size of array");
+    scanf("%d",&n);
+    input_array(arr,n);
+    printf("enter the position you want to delete");
+    scanf("%d",&p);
+    delete_array(n,p,arr);
 
+
+}
