f27b50ce8e58bc6d3064da976489e8263e771351
Tahan-Attin <tahanattin5@gmail.com>
2024-06-27 21:52:01 +0600

    Create Binary search.c

---
+++ Data Structure/Array/Binary search.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int arr[100];
+void input(int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+}
+ int binary_sarch(int size, int e)
+{
+    int l=0,r=size-1,i;
+    while(l<=r)
+    {
+        int mid=(l+r)/2;
+        if(arr[mid]==e)
+        {
+            return mid;
+        }
+        if(arr[mid]<e)
+        {
+            l=mid+1;
+        }
+        if(arr[mid]>e)
+        {
+            r=mid-1;
+        }
+
+    }
+}
+int main()
+{
+    int size,e;
+    printf("Enter the size of array");
+    scanf("%d",&size);
+    printf("enter the value you want to insert");
+    scanf("%d",&e);
+    input(size);
+    int k=binary_sarch(size,e);
+    printf("%d",k);
+}
