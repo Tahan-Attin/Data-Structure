46c10f73f3ffd5ef8f31867edd1f3845cb426a32
Tahan-Attin <tahanattin5@gmail.com>
2024-07-02 12:26:04 +0600

    Create Searching a linked list.c

---
+++ Data Structure/Linked List/Searching a linked list.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int info;
+    struct Node *link;
+
+}*start=NULL;
+
+typedef struct Node node;
+
+node *getNode(int item)
+{
+    node* new_node=(node*)malloc(sizeof(node));
+    new_node->info=item;
+    new_node->link =NULL;
+
+}
+
+void insertFirst(int item)
+{
+    node *new_node = getNode(item);
+    new_node->link = start;
+    start = new_node;
+}
+void traversal()
+{
+
+    node *ptr = start ;
+    while (ptr!=NULL)
+    {
+        printf("%d ",ptr->info);
+        ptr = ptr->link;
+    }
+}
+node *search(int item)
+{
+    node *ptr = start,*loc =NULL;
+    while (ptr!=NULL)
+    {
+        /*if(item<ptr->info)
+        {
+             ptr=ptr->link;
+
+        }*/
+         if(item==ptr->info)
+        {
+            loc=ptr;
+            break;
+        }
+        else
+            //break;
+            ptr =ptr->link;
+    }
+    return loc;
+
+}
+int main()
+{
+    for(int i=0;i<10;i++)
+    {
+
+        insertFirst(i+5);
+
+    }
+  traversal();
+  node *loc=search(9);
+  if(loc==NULL)
+    printf(" \n Not Found ");
+  else
+    printf("\n %d is found at %X ",loc->info,loc);
+    //insretLoc(loc,10);
+    //traversal();
+
+    return 0;
+}
