895d8362bdcb6bd31980829234700d3033c6b7fe
Tahan-Attin <tahanattin5@gmail.com>
2024-07-06 13:23:57 +0600

    Create Circular linkeed list inserting at end.c

---
+++ Data Structure/Linked List/Header linked list/Singly circular header linked list/Circular linkeed list inserting at end.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int data;
+    struct Node*link;
+};
+typedef struct Node node;
+node*tail;
+void addToEmpty(int data)
+{
+    node*temp=malloc(sizeof(node));
+    temp->data=data;
+    temp->link=temp;
+    tail=temp;
+}
+void addEnd(int data)
+{
+    node*newp=malloc(sizeof(node));
+    newp->data=data;
+    newp->link=tail->link;
+    tail->link=newp;
+    tail=newp;
+}
+void traversal()
+{
+    node*p=tail->link;
+    do
+    {
+        printf("%d",p->data);
+        p=p->link;
+    }while(p!=tail->link);
+}
+int main()
+{
+    addToEmpty(20);
+    addEnd(30);
+    addEnd(40);
+    traversal();
+}
