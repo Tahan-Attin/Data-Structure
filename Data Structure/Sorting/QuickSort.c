e4d5cb3af34e331dae29c1bf1a3b2ef66fe489db
Tahan-Attin <tahanattin5@gmail.com>
2025-01-19 12:11:19 +0600

    Update QuickSort.c

--- Data Structure/Sorting/QuickSort.c
+++ Data Structure/Sorting/QuickSort.c
@@ -1,43 +1,49 @@
 #include <stdio.h>
-int partition(int arr[],int low,int high){
+int partition(int arr[],int low,int high)
+{
     int p=arr[low];
     int i=low;
     int j=high;
-
-    while (i<j){
-        while(arr[i]<= p){
+    while (i<j)
+    {
+        while(arr[i]<= p)
+        {
             i++;
         }
-        while(arr[j] > p ){
+        while(arr[j]>p)
+        {
             j--;
         }
-        if(i < j){
-
-                 int temp=arr[i];
-                 arr[i]=arr[j];
-                 arr[j]=temp;
+        if(i<j)
+        {
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
         }
     }
-        int temp=arr[low];
-        arr[low]=arr[j];
-        arr[j]=temp;
-        return j;
+    int temp=arr[low];
+    arr[low]=arr[j];
+    arr[j]=temp;
+    return j;
 }
 
-void quickSort(int arr[],int low,int high){
-    if(low<high){
+void quickSort(int arr[],int low,int high)
+{
+    if(low<high)
+    {
         int pi=partition(arr,low,high);
         quickSort(arr,low,pi - 1);
         quickSort(arr,pi + 1,high);
     }
 }
 
-int main(){
+int main()
+{
     int t;
     printf("Enter the size of array");
     scanf("%d",&t);
     int arr[t];
-    for(int i=0;i<t;i++)
+    for(int i=0; i<t; i++)
     {
         scanf("%d",&arr[i]);
     }
