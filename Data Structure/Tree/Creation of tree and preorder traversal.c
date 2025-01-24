66fe9a61990ccd4b137ed5b513993c0f960d1545
Tahan-Attin <tahanattin5@gmail.com>
2025-01-25 01:52:38 +0600

    Create Creation of tree and preorder traversal.c

---
+++ Data Structure/Tree/Creation of tree and preorder traversal.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+struct node{
+    int data;
+    struct node*left;
+    struct node*right;
+};
+struct node*creat()
+{
+    int x;
+    struct node*nwep=malloc(sizeof(struct node));
+    printf("Enter Data , -1 For No node ");
+    scanf("%d",&x);
+    if(x==-1)
+    {
+        return 0;
+    }
+    nwep->data=x;
+    printf("Enter The Left Child of %d",x);
+    nwep->left=creat();
+    printf("Enter The Riht Child of %d",x);
+    nwep->right=creat();
+    return nwep;
+}
+void pre_order(struct node*root)
+{
+    if(root==0)
+    {
+        return;
+    }
+    printf("%d ",root->data);
+    pre_order(root->left);
+    pre_order(root->right);
+}
+int main()
+{
+    struct node*root;
+    root=creat();
+    pre_order(root);
+    return 0;
+}
